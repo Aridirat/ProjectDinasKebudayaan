9daa3224ac6e080c6081164798c85fb22363edf3
Ida Bagus Adimas <aridirat@gmail.com>
2025-03-16 23:59:23 +0800

    Menambahkan Component Footer

---
+++ dafabud/resources/views/components/footer.blade.php
@@ -0,0 +1,48 @@
+<footer id="footer" class="footer dark-background">
+
+    <div class="container footer-top">
+        <div class="row gy-4">
+            <div class="col-lg-4 col-md-6 footer-about">
+                <a href="/" class="logo d-flex align-items-center">
+                    <span class="sitename">Dinas Kebudayaan Badung</span>
+                </a>
+                <div class="footer-contact pt-3">
+                    <p>Ini adalah layanan pendaftaran kebudayaan benda dan tak benda di kabupaten Badung</p>
+                    <p class="mt-3"><strong>Alamat:</strong> Pusat Pemerintahan Mangupraja Mandala, Jl. Raya Sempidi Mengwi, Badung-Bali</p>
+                    <p><strong>Phone:</strong> <span>(0361) 9009273</span></p>
+                    <p><strong>Email:</strong> <span><a href="mailto:dinaskebudayaanbadung@gmail.com" class="text-light">dinaskebudayaanbadung@gmail.com</a></span></p>
+                </div>
+                <div class="social-links d-flex mt-4">
+                    <a href=""><i class="bi bi-twitter-x"></i></a>
+                    <a href=""><i class="bi bi-facebook"></i></a>
+                    <a href=""><i class="bi bi-instagram"></i></a>
+                    <a href=""><i class="bi bi-linkedin"></i></a>
+                </div>
+            </div>
+
+            <div class="offset-lg-2 col-lg-2 col-md-3 footer-links">
+                <h4>Pintasan</h4>
+                <ul>
+                    <li><i class="bi bi-chevron-right"></i> <a href="#">Rumah</a></li>
+                    <li><i class="bi bi-chevron-right"></i> <a href="#">Kebudayaan</a></li>
+                </ul>
+            </div>
+
+            <div class="col-lg-2 col-md-3 footer-links">
+                <h4>Formulir Pendaftaran</h4>
+                <ul>
+                    <li><i class="bi bi-chevron-right"></i> <a href="#">Kebudayaan Benda</a></li>
+                    <li><i class="bi bi-chevron-right"></i> <a href="#">Kebudayaan Tak Benda</a></li>
+                </ul>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="container copyright text-center mt-4">
+        <p>© <span>Copyright 2025</span> <strong class="px-1 sitename">Pemerintah Kabupaten Badung.</strong> <span>All Rights Reserved.</span></p>
+        <div class="credits">
+        </div>
+    </div>
+
+</footer>
